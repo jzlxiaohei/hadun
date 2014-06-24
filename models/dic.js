aa1413d131baf36c718056fb1b176fb0ed3abc5f
zilong <jzlxiaohei@163.com>
2014-06-24 18:20:58 +0800

    dic.html

--- models/dic.js
+++ models/dic.js
@@ -1,11 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
     var Dic = sequelize.define('Dic', {
-        group: DataTypes.STRING,
+        group: DataTypes.STRING,//中英文皆可
         key: DataTypes.STRING,
         value: DataTypes.STRING
     }, {
         timestamps: false
     });
-
     return Dic;
 }
